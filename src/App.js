cf8e78e52e7bcd4666d359e644bee9dbc5ce92ee
Jeffrey Martin <unicycle77@gmail.com>
2021-01-30 11:40:40 -0800

    base pokedex project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import PokeGame from './PokeGame';
+
+function App() {
+  return (
+    <div className="App">
+      <PokeGame />
+    </div>
+  );
+}
+
+export default App;
